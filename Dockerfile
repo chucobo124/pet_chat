b86bb9a4930c308f510bdef71e88e5cc7cdbdbcc
Lucas Chen <chucobo5219@gmail.com>
2017-12-27 09:36:54 +0800

    fix in the middle

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ WORKDIR /usr/src/pet_chat
 
 COPY package*.json ./
 RUN npm install --save
+# TODO: Need to seperate when ENV completed
+RUN npm install -g nodemon
 COPY . .
 EXPOSE 8080
-CMD ["npm", "start"]
+# TODO: Need to seperate when ENV completed
+# CMD ["npm", "start"]
+CMD nodemon
--- server.js
+++ server.js
@@ -3,34 +3,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    // channel = req.query.channel
-    // io.of('/' + channel).on('connection', function (socket) {
-    //     socket.on(channel, function (data) {
-    //         io.emit(channel, data);
-    //         console.log(data);
-    //     });
-    // });
+    room = req.query.room
+
+    io.on('connection', function (socket) {
+        socket.join(room)
+    });
+
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-        console.log('socket:' + socket)
-        console.log('message: ' + msg);
-    });
-});
-
 // io.on('connection', function (socket) {
-//     socket.emit('news', { hello: 'world' });
-//     socket.on('my other event', function (data) {
-//         io.emit('my other event', data);
-//         console.log(data);
+//     socket.on('chat message', function (msg) {
+//         io.emit('chat message', msg);
+//         console.log('socket:' + socket)
+//         console.log('message: ' + msg);
 //     });
 // });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+io.on('connection', function (socket) {
+    var handshake = socket.handshake
+    console.log(handshake)
+    socket.emit('news', { hello: 'world' });
+    socket.on('chat message', function (data) {
+        io.emit('chat message', data);
+        console.log(data);
+    });
+});
+
+http.listen(8080, function () {
+    console.log('listening on *:8080');
 });
 
